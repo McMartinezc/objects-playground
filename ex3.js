8caacb7b9cab60f674355b7dbbb61a592478a097
McMartinezc <psyence_mc@hotmail.com>
2023-10-17 11:28:11 +0200

    finalizado

--- ex3.js
+++ ex3.js
@@ -19,6 +19,8 @@ let estudiante = {
  * 
  * Pistas:
  * 
- * 1. ¿De tipo de variable es 'conocimientos'?
+ * 1. ¿De tipo de variable es 'conocimientos'? 
  * 2. ¿Cuál es el método para añadir un nuevo elemento a un array?
  */
+estudiante.conocimientos.push("CSS");
+console.log(estudiante);
